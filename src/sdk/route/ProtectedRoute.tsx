770674236e26f712826d80be6e9ab1f056190af0
soohanlee <soohan0808@gmail.com>
2025-06-17 20:53:42 +0900

    ProtectedRoute 코드 sdk 사용법 변경에 따른 수정

--- src/sdk/route/ProtectedRoute.tsx
+++ src/sdk/route/ProtectedRoute.tsx
@@ -1,29 +1,52 @@
 import { Navigate, useLocation, Route } from "react-router-dom";
 import type { PathRouteProps } from "react-router-dom";
 import { useAuth } from "@/sdk/useAuth";
+import { useMemo } from "react";
 
 interface ProtectedRouteProps extends Omit<PathRouteProps, "element"> {
-  enabled: string[];
+  enabled: string[]; // roleId
+  authAssetId: string | null;
   component: React.ComponentType<object>;
+  fallback?: React.ReactNode;
 }
 
 export const ProtectedRoute = ({
+  authAssetId,
   enabled,
   component: Component,
+  fallback,
   ...routeProps
 }: ProtectedRouteProps) => {
   const { sdk } = useAuth();
   const location = useLocation();
 
-  const ProtectedElement = () => {
-    const hasRequiredRoles = enabled.every((role) => sdk.hasRole(role));
+  const ProtectedElement = useMemo(() => {
+    return () => {
+      // 로그인 체크
+      if (!sdk.getAccessToken()) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+      }
 
-    if (!hasRequiredRoles) {
-      return <Navigate to="/login" state={{ from: location }} replace />;
-    }
+      // 권한 체크
+      try {
+        const hasRequiredRoles = enabled.every((roleId) =>
+          sdk.isAuthorized(authAssetId, roleId)
+        );
 
-    return <Component />;
-  };
+        if (!hasRequiredRoles) {
+          if (fallback) {
+            return <>{fallback}</>;
+          }
+          return <Navigate to="/login" state={{ from: location }} replace />;
+        }
+
+        return <Component />;
+      } catch (error) {
+        console.error("권한 확인 중 오류 발생:", error);
+        return <Navigate to="/login" state={{ from: location }} replace />;
+      }
+    };
+  }, [sdk, Component, location, enabled, authAssetId, fallback]);
 
   return <Route {...routeProps} element={<ProtectedElement />} />;
 };
