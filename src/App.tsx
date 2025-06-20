a2c666342642cf09d3026aeab12fb5fe61cde1bc
soohanlee <soohan0808@gmail.com>
2025-06-20 17:05:23 +0900

    app.tsx styled components 제거

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,20 @@ import { ProtectedRoute } from "@/sdk/route/ProtectedRoute";
 /*PackageImport*/
 
 const Container = styled.div`
-  min-height: 100vh;
   background-color: ${({ theme }) => theme.colors.background.secondary};
   font-family: ${({ theme }) => theme.fontFamily.sans};
-`;
-
-const Main = styled.main`
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: ${({ theme }) => theme.spacing.large};
-  color: ${({ theme }) => theme.colors.text.white};
-
-  @media (max-width: 768px) {
-    padding: ${({ theme }) => theme.spacing.medium};
-  }
 `;
 
 function App() {
   return (
     <Container>
-      <Main>
-        <Routes>
-          {/*PackageRoutes*/}
+      <Routes>
+        {/*PackageRoutes*/}
 
-          <Route path="/" element={<WelcomeServerkit />} />
-          <Route path="/500" element={<ServerError />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Main>
+        <Route path="/" element={<WelcomeServerkit />} />
+        <Route path="/500" element={<ServerError />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Container>
   );
 }
