0b0d063ed20639259adf545e1cecde5cbb45b2f5
LKH <rovr0015@gmail.com>
2025-07-11 13:00:34 +0900

    mediaMapper 업데이트

--- src/hooks/mediaMapper.ts
+++ src/hooks/mediaMapper.ts
@@ -21,14 +21,23 @@ export interface MediaEntry {
   mediaSrcData: mediaSrcData[];
 }
 
+interface mediaPathMap {
+  path: string;
+  type: string;
+  recommendedSize: RecommendedSize;
+  mediaType: MediaType;
+}
+
 export function createMediaMap(mediaConfig: MediaEntry[]) {
-  const mediaPathMap: Record<string, { path: string; type: string }> = {};
+  const mediaPathMap: Record<string, mediaPathMap> = {};
 
   for (const item of mediaConfig) {
     for (const media of item.mediaSrcData) {
       mediaPathMap[media.name] = {
         path: media.data,
         type: media.type,
+        recommendedSize: item.recommendedSize,
+        mediaType: item.type,
       };
     }
   }
@@ -37,9 +46,19 @@ export function createMediaMap(mediaConfig: MediaEntry[]) {
     const filename = mediaPathMap[key];
     if (!filename) return "";
     if (filename.type === "import") {
-      return getMediaSrcFromManager(filename.path) || "";
+      return (
+        getMediaSrcFromManager(filename.path) || getMediaDefaultSrc(filename)
+      );
     } else {
-      return filename.path;
+      return filename.path != "" ? filename.path : getMediaDefaultSrc(filename);
+    }
+  }
+
+  function getMediaDefaultSrc(fileData: mediaPathMap): string {
+    if (fileData.mediaType === "image") {
+      return `https://placehold.co/${fileData.recommendedSize.width}x${fileData.recommendedSize.height}`;
+    } else {
+      return "https://2050today.org/wp-content/uploads/2020/07/Video-Placeholder.mp4";
     }
   }
 
